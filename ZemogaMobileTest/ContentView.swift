fe57481d583c1ddc63a24abd159f1bffc2dff532
Rone Shender Loza Aliaga <rone.loza@gmail.com>
2022-05-11 20:23:02 -0500

    Initial Commit

---
+++ ZemogaMobileTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ZemogaMobileTest
+//
+//  Created by Rone Shender Loza Aliaga on 11/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ZemogaMobileTest/ZemogaMobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZemogaMobileTestApp.swift
+//  ZemogaMobileTest
+//
+//  Created by Rone Shender Loza Aliaga on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct ZemogaMobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
